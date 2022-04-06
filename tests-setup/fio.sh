b9f2e66bb7862161947507d08ce7d11a9701f06f
Alice Frosi <afrosi@redhat.com>
2022-04-06 17:25:42 +0200

    Run script for fio setup

---
+++ tests-setup/fio.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x
+
+device=device-to-test
+OUTPUT_DIR=/output
+if [  $(lsblk | grep "$device") -ne 0 ]; then 
+    echo "Device $device not found"
+    exit 1
+fi
+
+mkdir -p $OUTPUT_DIR
+# Preallocate disks to have consistent tests
+size=$(lsblk |grep $device |awk '{ print $4 }')
+fallocate -l $size $device
+
+fio /fio-jobs/*.fio --output $OUTPUT_DIR/fio.log
