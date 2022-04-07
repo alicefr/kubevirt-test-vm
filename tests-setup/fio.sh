50db502fc837fa2ab9cd36c3ecfd7e2994c8de66
Alice Frosi <afrosi@redhat.com>
2022-04-07 11:10:23 +0200

    Use posix fallocate to preallocate the device

--- tests-setup/fio.sh
+++ tests-setup/fio.sh
@@ -12,6 +12,6 @@ fi
 mkdir -p $OUTPUT_DIR
 # Preallocate disks to have consistent tests
 size=$(lsblk |grep $device |awk '{ print $4 }')
-fallocate -l $size $device
+fallocate -l $size -x $device
 
 fio /fio-jobs/*.fio --output $OUTPUT_DIR/fio.log
