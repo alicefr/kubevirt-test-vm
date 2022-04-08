3c976263cc43b120c0ec5511c8f23c5c80e1ea77
Alice Frosi <afrosi@redhat.com>
2022-04-08 09:20:36 +0200

    Fix size parsing

--- tests-setup/fio.sh
+++ tests-setup/fio.sh
@@ -2,16 +2,21 @@
 
 set -x
 
-device=device-to-test
+device=/dev/device-to-test
 OUTPUT_DIR=/output
-if [  $(lsblk | grep "$device") -ne 0 ]; then 
+lsblk | grep "$device"
+if [ $? -ne 0 ]; then
     echo "Device $device not found"
     exit 1
 fi
 
 mkdir -p $OUTPUT_DIR
 # Preallocate disks to have consistent tests
-size=$(lsblk |grep $device |awk '{ print $4 }')
+size=$(lsblk -n -o SIZE $device)
+if [ -z "$size" ]; then
+	echo "Failed parising the size of the device"
+	exit 1
+fi
 fallocate -l $size -x $device
 
 fio /fio-jobs/*.fio --output $OUTPUT_DIR/fio.log
